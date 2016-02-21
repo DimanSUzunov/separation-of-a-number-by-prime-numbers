90e9d60b99cae3301c6392341edb317fbf1b1f17
Diman Uzunov <stas_uzunov@hotmail.com>
2016-02-21 23:27:16 +0200

    Added files via upload
    This program is a  C# console app written in Visual Studio so make sure that you have VS before opening.

---
+++ razdelqnenachislonaprostimnojiteli/razdelqnenachislonaprostimnojiteli/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razdelqnenachislonaprostimnojiteli
+{
+    class Program
+    {
+        static void firstfiftyprimenumbers(short[] a)
+        {
+            bool flag = true;
+            a[0] = 2;
+            for (short i = 2, k = 1; k < 50; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    a[k] = i;
+                    k++;
+                }
+                else { flag = true; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+          
+                short[] primenumbers = new short[50];
+            firstfiftyprimenumbers(primenumbers);
+                for (int i = 0; N != 1;)
+                {
+                    if (N % primenumbers[i] == 0)
+                    {
+                        N /= primenumbers[i];
+                        Console.Write(primenumbers[i] + " ");
+                    }
+                    else { i++; }
+                }
+            
+    
+            Console.ReadKey();
+        }
+    }
+}
